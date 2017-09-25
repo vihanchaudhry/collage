5756a3fff347f671c415c84486b5c4529bd383fd
Vihan Chaudhry <vihanchaudhry@gmail.com>
2017-09-24 17:48:49 -0700

    Users' profiles show all their posts

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
+const Post = require('../models/post')
 
 // TEMPORARY
 // Json payload of all users
@@ -20,11 +21,13 @@ router.get('/', (req, res) => {
 router.get('/:username', 
   (req, res) => {
     User.findOne({ username: req.params.username }, (err, user) => {
-      if (err) { 
-        res.send(err) 
-      } else {
-        res.render('user', { user: user })
-      } 
+      if (err) { res.send(err) }
+      // Get all posts made by the user
+      Post.find({ user: user.username }, (err2, posts) => {
+        console.log(posts)
+        if (err2) { res.send(err2) }
+        res.render('user', { user: user, posts: posts }) 
+      })
     })
   }
 )
